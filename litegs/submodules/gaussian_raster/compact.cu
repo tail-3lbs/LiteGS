2d249259fd35695cdac6a1105fa35b39ecfeae30
Jiaqi Liu <jiaqi.3031@gmail.com>
2025-08-11 16:39:28 +0800

    fix: to run

--- litegs/submodules/gaussian_raster/compact.cu
+++ litegs/submodules/gaussian_raster/compact.cu
@@ -284,7 +284,7 @@ void adamUpdate(torch::Tensor &param,torch::Tensor &param_grad,torch::Tensor &ex
     else if(param.sizes().size() == 2)
     {
         int primitive_num=visible.size(0);
-        sparse_primitive_adam_kernel<<<int(std::ceilf(primitive_num / 256.0f)),256>>> (
+        sparse_primitive_adam_kernel<<<int(ceilf(primitive_num / 256.0f)),256>>> (
             param.packed_accessor32<float, 2, torch::RestrictPtrTraits>(),
             param_grad.packed_accessor32<float, 2, torch::RestrictPtrTraits>(),
             exp_avg.packed_accessor32<float, 2, torch::RestrictPtrTraits>(),
